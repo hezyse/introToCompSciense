eeb9462982eacd1c76c0a2257b2f0060bb9120de
hezyse <75539333+hezyse@users.noreply.github.com>
2023-06-19 00:05:31 +0300

    Update Ex13.java

--- Ex13.java
+++ Ex13.java
@@ -25,6 +25,24 @@ public class Ex13 {
         // Skip the current element and move to the next index
         return isSum(a, num, index + 1, 1);
     }
+    
+    public static int missingValue (int [] arr) {
+        int length = arr.length;
+        int delta = arr[1] - arr[0];
+        int retVal = Integer.MAX_VALUE;
+        for (int i=2; i < length; i++) {
+            int curDelta = arr[i] - arr[i - 1];
+            if (curDelta / delta == 0) {
+                retVal = arr[i - 1] - curDelta;
+                break;
+            }
+            else if (curDelta / delta == 2) {
+                retVal = arr[i] - delta;
+                break;
+            }
+        }
+        return retVal;
+    }
 
 
     
