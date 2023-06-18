8b005b9b43339a381f7d1e031ac9fed579ed4dd4
hezyse <75539333+hezyse@users.noreply.github.com>
2023-06-19 00:06:01 +0300

    Update Ex13.java

--- Ex13.java
+++ Ex13.java
@@ -43,6 +43,39 @@ public class Ex13 {
         }
         return retVal;
     }
+    
+     public static int longestPalindrome(int[] arr) {
+        return longestPalindrome(arr, 0, arr.length - 1, false);
+    }
+
+
+    public static int longestPalindrome(int[] arr, int start, int end, boolean previousMatched) {
+        // Base case: If the start index is equal to or greater than the end index, it's a palindrome of length 1 or 0
+        if (start >= end) {
+            int returnSize = end - start + 1;
+            return returnSize;
+        }
+
+        // Recursive case: Check if the first and last elements are the same
+        if (arr[start] == arr[end]) {
+            // If the first and last elements are the same, recursively check the sequence without the first and last elements
+            int next_inner = longestPalindrome(arr, start + 1, end - 1, true);
+            int innerPolindrome = 2 + next_inner;
+            if (innerPolindrome != 2 || start + 1 >= end - 1)
+                return innerPolindrome;
+            else
+                return 0;
+        } else if (arr[start] != arr[end] && previousMatched) {
+            return 0;
+        } else {
+            // If the first and last elements are not the same, recursively check the sequence without the last element
+            int length1 = longestPalindrome(arr, start, end - 1, false);
+            // Recursively check the sequence without the first element
+            int length2 = longestPalindrome(arr, start + 1, end, false);
+            // Return the maximum length between the two cases
+            return Math.max(length1, length2);
+        }
+    }
 
 
     
