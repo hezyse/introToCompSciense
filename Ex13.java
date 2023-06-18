fc5a80cc96e8f9913b45e619407dc4aed9219bb4
hezyse <75539333+hezyse@users.noreply.github.com>
2023-06-19 00:06:31 +0300

    Update Ex13.java

--- Ex13.java
+++ Ex13.java
@@ -76,6 +76,46 @@ public class Ex13 {
             return Math.max(length1, length2);
         }
     }
+    
+     public static int shortestRoad(int[] road1, int[] road2) {
+        int n = road1.length;
+        int totalTime = 0;
+
+        // Calculate the total time if the driver stays on road 1 without switching
+        for (int i = 0; i < n; i++) {
+            totalTime += road1[i];
+        }
+
+        // Calculate the total time if the driver switches from road 1 to road 2
+        // Find the minimum time to switch by comparing the time at each index
+        int switchTime = Integer.MAX_VALUE;
+        int currentTotalTime = 0;
+        for (int i = 0; i < n; i++) {
+            currentTotalTime += road1[i];
+            int remainingTime = getTotalTime(road2, i + 1, n);
+            switchTime = Math.min(switchTime, currentTotalTime + remainingTime);
+        }
+
+        // Calculate the total time if the driver switches from road 2 to road 1
+        // Find the minimum time to switch by comparing the time at each index
+        currentTotalTime = 0;
+        for (int i = 0; i < n; i++) {
+            currentTotalTime += road2[i];
+            int remainingTime = getTotalTime(road1, i + 1, n);
+            switchTime = Math.min(switchTime, currentTotalTime + remainingTime);
+        }
+
+        // Return the minimum time among the three possibilities
+        return Math.min(totalTime, switchTime);
+    }
+
+    private static int getTotalTime(int[] road, int startIndex, int endIndex) {
+        int totalTime = 0;
+        for (int i = startIndex; i < endIndex; i++) {
+            totalTime += road[i];
+        }
+        return totalTime;
+    }
 
 
     
