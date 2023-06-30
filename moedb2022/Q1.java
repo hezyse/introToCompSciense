1aefa8b2255cd80215640e63fbe29b72986d2e22
Hezis <radware>
2023-06-30 20:15:26 +0300

    fixed the issues

--- moedb2022/Q1.java
+++ moedb2022/Q1.java
@@ -1,41 +1,34 @@
 public class Q1 {
     public static int calc(int num, int result, int maxOp) {
-        if (maxOp == 0) {
-            if (num == result) {
-                System.out.println(num);
+        return calc(num, num, result, maxOp, String.valueOf(num));
+    }
+    public static int calc(int num, int cur_result, int result, int maxOp, String expression) {
+        if (maxOp >= 0 && cur_result == result) {
+
+                System.out.println(expression + " = " + result);
                 return 1;
-            }
-            return 0;
+
         }
 
         int count = 0;
+        if (maxOp > 0) {
+            count += calc(num, cur_result + num, result, maxOp - 1, expression + " + " + num);
+            count += calc(num, cur_result - num, result, maxOp - 1, expression + " - " + num);
+            count += calc(num, cur_result * num, result, maxOp - 1, expression + " * " + num);
 
-        // Add
-        count += calc(num, result - num, maxOp - 1);
-        System.out.println(num + " + " + calc(num, result - num, maxOp - 1));
-
-        // Sub
-        count += calc(num, result + num, maxOp - 1);
-        System.out.println(num + " - " + calc(num, result + num, maxOp - 1));
-
-        // *
-        if (num != 0 && result % num == 0) {
-            count += calc(num, result / num, maxOp - 1);
-            System.out.println(num + " * " + calc(num, result / num, maxOp - 1));
+            if (num != 0 ) {
+                count += calc(num, cur_result / num, result, maxOp - 1, expression + " / " + num);
+            }
         }
 
-        // /
-        if (result != 0 && num % result == 0) {
-            count += calc(num, result * num, maxOp - 1);
-            System.out.println(num + " / " + calc(num, result * num, maxOp - 1));
-        }
-
-        return count;  // מספר הב
-
+        return count;
     }
     public static void main(String[] args) {
-        int totalOptions = calc(3, 36, 5);
-        System.out.println(totalOptions);
+        int num = 5;
+        int result = 6;
+        int maxOp = 4;
+        int totalCount = calc(num, result, maxOp);
+        System.out.println("Total valid expressions: " + totalCount);
     }
 
 }
