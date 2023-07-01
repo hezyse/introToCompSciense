773220d0fe95b5312227264614489709c511e7a9
Hezis <radware>
2023-07-01 21:52:24 +0300

    solved Q1 2018A

--- 2018A/Q12018.java
+++ 2018A/Q12018.java
@@ -1,11 +1,21 @@
-public class Q1{
+public class Q12018{
   public static int  howManySorted(int n, int max){
 
     return howManySorted(n, max, 0, 1, "");
   }
 
   public static int howManySorted(int n, int max, int curIndex, int curNum, String expression){
-    return 0;
+
+    if(curIndex == n){
+      System.out.println(expression);
+      return 1;
+    }
+    int count = 0;
+    if(curNum <= max ){
+      count += howManySorted(n, max, curIndex+1, curNum, expression + " " + curNum);
+      count += howManySorted(n, max, curIndex, curNum+1, expression);
+    }
+    return count;
   }
 
   public static void main(String[] args) {
