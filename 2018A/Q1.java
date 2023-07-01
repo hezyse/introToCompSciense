3ca3e7ed55d0333bb006ab88ff097d251d98be07
Hezis <radware>
2023-07-01 20:53:21 +0300

    added new method

--- 2018A/Q1.java
+++ 2018A/Q1.java
@@ -1,5 +1,15 @@
 public class Q1{
   public static int  howManySorted(int n, int max){
+
+    return howManySorted(n, max, 0, 1, "");
+  }
+
+  public static int howManySorted(int n, int max, int curIndex, int curNum, String expression){
     return 0;
+  }
+
+  public static void main(String[] args) {
+    int total = howManySorted(2,3);
+    System.out.println("Total is: " + total);
   }
 }
