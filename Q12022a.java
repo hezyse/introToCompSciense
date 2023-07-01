81cdb0e31fe33ef75de84149ddedcb66fe19783b
Hezis <radware>
2023-07-01 22:27:38 +0300

    added new question

---
+++ Q12022a.java
@@ -0,0 +1,9 @@
+public class Q12022a {
+    public static int lengthPath(char[][] mat, String pattern){
+        return 0;
+    }
+
+    public static void main(String[] args) {
+        System.out.println("hello");
+    }
+}
